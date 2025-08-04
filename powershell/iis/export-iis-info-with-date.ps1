262bdd2b7874814ed9309ed03156ffa99d473a10
Abelardo Irarrazabal <abelardo.irarrazabal@security.cl>
2025-08-04 12:36:04 -0400

    script de iis con fecha

---
+++ powershell/iis/export-iis-info-with-date.ps1
@@ -0,0 +1,55 @@
+Write-Host "Inicio - Script"
+$Websites = Get-Website
+$Hostname = hostname
+$fechaReporte = (Get-Date).ToString('yyyy/MM/dd')
+$filename = "apps-"+$Hostname+".txt"
+
+'Hostname|Type|Site|Webapp_Name|State|PhysicalPath|Bindings|AppPool_Name|AppPool_State|AppPool_ManagedRuntimeVersion|Fecha_Reporte' > $filename
+
+foreach ($Website in $Websites) {
+
+    $AppPool = Get-IISAppPool -Name $Website.ApplicationPool
+
+    $customWeb = [PSCustomObject]@{
+        Hostname = $Hostname
+        Type = 'Site'
+        Site = $Website.Name -join ';'
+        Webapp_Name = ''
+        State                 = $Website.State -join ';'
+        PhysicalPath          = $Website.PhysicalPath
+        Bindings              = $Website.Bindings.Collection -join ';'
+        AppPool_Name                  = $AppPool.Name -join';'
+        AppPool_State                 = $AppPool.State -join ';'
+        AppPool_ManagedRuntimeVersion = $AppPool.ManagedRuntimeVersion -join ';'
+        Fecha_Reporte = $fechaReporte
+
+    }
+
+    $customWeb.Hostname + '|' +$customWeb.Type + '|' + $customWeb.Site + '|' + $customWeb.Webapp_Name + '|' + $customWeb.State + '|' + $customWeb.PhysicalPath + '|' + $customWeb.Bindings + '|' + $customWeb.AppPool_Name + '|' + $customWeb.AppPool_State + '|' + $customWeb.AppPool_ManagedRuntimeVersion + '|' + $customApp.Fecha_Reporte >> $filename
+}
+
+$Webapps = Get-WebApplication
+
+foreach ($Webapp in $Webapps) {
+    $name = ($Webapp.Path).Replace('/','')
+    $site = $Webapp.GetParentElement().Attributes['name'].Value
+    $AppPool = Get-IISAppPool -Name $Webapp.ApplicationPool
+
+    $customApp = [PSCustomObject]@{
+        Hostname = $Hostname
+        Type = 'Application'
+        Site = $site
+        Webapp_Name  = $name
+        State = ''
+        PhysicalPath = $Webapp.PhysicalPath
+        Bindings = ''
+        AppPool_Name                  = $AppPool.Name -join';'
+        AppPool_State                 = $AppPool.State -join ';'
+        AppPool_ManagedRuntimeVersion = $AppPool.ManagedRuntimeVersion -join ';'
+        Fecha_Reporte = $fechaReporte
+    }
+
+    $customApp.Hostname + '|' +$customApp.Type + '|' + $customApp.Site + '|' + $customApp.Webapp_Name + '|' + $customApp.State + '|' + $customApp.PhysicalPath + '|' + $customApp.Bindings + '|' + $customApp.AppPool_Name + '|' + $customApp.AppPool_State + '|' + $customApp.AppPool_ManagedRuntimeVersion + '|' + $customApp.Fecha_Reporte >> $filename
+}  
+Write-Host $filename
+Write-Host "Fin - Script"
